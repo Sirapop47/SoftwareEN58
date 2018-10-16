3269ba580d41480f7f8eca55801d53b8b45056ed
Sirapop47 <sirpopp58@email.nu.ac.th>
2018-10-16 11:13:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const path = require('path')
 const app = express()
 const port = 8000
 
-app.use(express.static(path.join(__dirname, 'Publie')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/*', (req, res) =>
-	 res.sendFile(path.join(__dirname, 'Publie', 'Resume.html')));
+	 res.sendFile(path.join(__dirname, 'public', 'Resume.html')));
 
 app.listen(port, () => console.log('app listening on port'+port))
 
